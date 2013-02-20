90acb788a79b1bca930b976b970155789ddef94d
Wolff09 <s_wolff09@cs.uni-kl.de>
2013-02-20 13:57:01 +0100

    edge types?

--- convert_mb.py
+++ convert_mb.py
@@ -6,25 +6,39 @@ def main():
 	G = nx.Graph()
 
 	print "-- reading nodes --"
-	attrs_file = open("musicbrainzGraph/musicbrainzNodes.csv", "r")
+	attrs_file = open("private_data/musicbrainzGraph/musicbrainzNodes.csv", "r")
 	my_reader = csv.reader(attrs_file, delimiter="\t")
 	my_reader.next() # pop head
+	num = {}
 	for row in my_reader:
 		id = int(row[0])
-		G.add_node(id, globalMusicBrainzId=row[1], musicBrainzTableId=row[2], nodeType=row[3], name=row[4])
+		#G.add_node(id)#, globalMusicBrainzId=row[1], musicBrainzTableId=row[2], nodeType=row[3], name=row[4])
 		if id % 100000 == 0:
 			print id
+		nodeType = row[3]
+		if nodeType in num:
+			num[nodeType] += 1
+		else:
+			num[nodeType] = 1
+	print str(num).replace(",", ",\n")
 
 	print "-- reading edges --"
-	attrs_file = open("musicbrainzGraph/musicbrainzRelations.csv", "r")
+	attrs_file = open("private_data/musicbrainzGraph/musicbrainzRelations.csv", "r")
 	my_reader = csv.reader(attrs_file, delimiter="\t")
 	my_reader.next() # pop head
 	index = 0
+	num = {}
 	for row in my_reader:
-		G.add_edge(int(row[0]), int(row[1]), lLinkTypeId=row[2], strShortLinkPhrase=row[3])
+		G.add_edge(int(row[0]), int(row[1]))#, lLinkTypeId=row[2], strShortLinkPhrase=row[3])
 		if index % 100000 == 0:
 			print index
 		index += 1
+		edgeType = int(row[2])
+		if edgeType in num:
+			num[edgeType] += 1
+		else:
+			num[edgeType] = 1
+	print str(num).replace(",", ",\n")
 
 	print "-- writing graph --"
 	nx.write_gexf(G, 'musicbrainz.gexf')
