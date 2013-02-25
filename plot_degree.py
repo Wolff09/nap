d0439811022f4441a1098bc4646b28f716dfab11
Wolff09 <s_wolff09@cs.uni-kl.de>
2013-02-25 14:24:53 +0100

    some changes and test... i do not remember what i have done but committing is cool

--- plot_degree.py
+++ plot_degree.py
@@ -1,18 +1,24 @@
 from read_csv import read_csv as read
+import matplotlib
+import pylab
 
 def main():
 	counter = {} # nodeId: degree
-	for data in read(path):
+	for data in read("private_data/musicbrainzGraph/musicbrainzRelations.csv"):
 		left = int(data['nodeId0'])
 		right = int(data['nodeId1'])
 		counter[left] = counter.get(left, 0) + 1
 		counter[right] = counter.get(right, 0) + 1
 	
-	distribution = {}
+	m = max(counter.values())
+	l = [key for key, val in counter if val == m] # iter over key, value pairs not by default!!
+	print l
+
+	distribution = {} # val: number of occurrence
 	for val in counter.values():
 		distribution[val] = distribution.get(val, 0) + 1
 
-	# distribution contains val: number of occurrence
+	print str(distribution).replace(",", ",\n")
 
 if __name__ == '__main__':
 	main()
