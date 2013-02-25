db8e668923942687a14533900557d0325518e231
Wolff09 <s_wolff09@cs.uni-kl.de>
2013-02-25 14:23:50 +0100

    reading nodes now should work even if the file is partly malformed

--- read_csv.py
+++ read_csv.py
@@ -5,10 +5,16 @@ def read_csv(path, delimiter="\t"):
 	Reads a CSV file with given path (may be relative).
 	Yields every row as a dictionary.
 	The keys for the dictionary are read from the head of the CSV file.
+
+	If a row contains more fields than the header indicates the last
+	field of the head consumes the remaining fields of the row.
 	"""
 	file = open(path, "r")
 	reader = csv.reader(file, delimiter=delimiter)
 	head = reader.next() # pop head
+	len_head = len(head)
+	len_head_sub = len_head - 1
 	for row in reader:
-		data = {head[i]: row[i] for i in range(0, len(row))}
+		data = {head[i]: row[i] for i in range(0, len_head_sub)}
+		data[head[-1]] = " ".join(row[i] for i in range(len_head, len(row))) # last field greedily takes the remain
 		yield data
