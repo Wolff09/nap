23637753c14ce11fe61a04e4b1321796a4298037
Wolff09 <s_wolff09@cs.uni-kl.de>
2013-03-02 17:20:43 +0100

    written some shortcuts...

---
+++ shortcuts.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import networkx as nx
+from networkx import centrality, distance_measures
+from collections import Counter
+
+
+################################### degrees ###################################
+def degree(graph):
+	return [int(graph.degree(node)) for node in graph]
+
+def degree_distribution(graph, *args, **kwargs):
+	return Counter(degree(graph, *args, **kwargs)) # Counter is a dict subclass
+
+
+################################## distances ##################################
+# see: http://networkx.github.com/documentation/latest/reference/algorithms.distance_measures.html
+
+def diameter(graph, *args, **kwargs):
+	return distance_measures.diameter(graph, *args, **kwargs)
+
+def eccentricity(graph, *args, **kwargs):
+	return distance_measures.eccentricity(graph, *args, **kwargs)
+
+
+################################# clustering ##################################
+# see: http://networkx.github.com/documentation/latest/reference/algorithms.clustering.html
+
+def clustering(graph, *args, **kwargs):
+	return nx.clustering(graph, *args, **kwargs)
+
+def average_clustering(graph, *args, **kwargs):
+	return nx.average_clustering(graph, *args, **kwargs)
+
+def transitivity(graph, *args, **kwargs):
+	return nx.transitivity(graph, *args, **kwargs)
+
+
+################################# centrality ##################################
+# see: http://networkx.github.com/documentation/latest/reference/algorithms.centrality.html
+
+def degree_centrality(graph, *args, **kwargs):
+	return centrality.degree_centrality(graph, *args, **kwargs)
+
+def closeness_centrality(graph, *args, **kwargs):
+	return centrality.closeness_centrality(graph, *args, **kwargs)
+
+def betweenness_centrality(graph, *args, **kwargs):
+	return centrality.betweenness_centrality(graph, *args, **kwargs)
