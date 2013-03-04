9cb7b870d534155712eb1cd1bea0ca67e7545223
Wolff09 <s_wolff09@cs.uni-kl.de>
2013-03-04 15:02:08 +0100

    öhh döhh sebi...

--- find_partitions.py
+++ find_partitions.py
@@ -4,6 +4,7 @@
 from read_csv import read_as_array as read
 from union_find import *
 import os, shutil
+from collections import Counter
 
 
 # configuration
@@ -73,44 +74,47 @@ def main(length=0):
 	########################## find remaining partitions ###########################
 	print "\n>>> Searching for partitions..."
 	sets = [Find(node).id for node in nodes]
+	c = Counter(sets)
 	# del nodes # release memory
-	remaining = []
-	counter = 0
-	for p in sets:
-		if not p in remaining:
-			remaining += [p]
-		counter += 1
-		if counter % BARRIER_SEARCH_PARTS == 0: print "..searching for partitions (%s, %s)" % (counter, len(remaining))
-	print "Number of Partitions: %s" % len(remaining)
-	print "Partitions (id of one contained node): %s" % remaining
+	# remaining = []
+	# counter = 0
+	# for p in sets:
+	#	if not p in remaining:
+	#		remaining += [p]
+	#	counter += 1
+	#	if counter % BARRIER_SEARCH_PARTS == 0: print "..searching for partitions (%s, %s)" % (counter, len(remaining))
+	# print "Number of Partitions: %s" % len(remaining)
+	# print "Partitions (id of one contained node): %s" % remaining
+	print "Number of Partitions: %s" % len(c)
+	print "Partitions (id of one contained node): \n%s" % str(c).replace(",", ",\n")
 
 
 	##################### write partitions to dedicated files ######################
-	print "\n>>> Start writing partitions..."
-	shutil.rmtree(PATH_TO_OUTPUT_DIR, ignore_errors=True)
-	os.mkdir(PATH_TO_OUTPUT_DIR)
-	def make_path(partition):
-		return os.path.join(PATH_TO_OUTPUT_DIR, '%s.csv' % partition)
-	files = {partition: open(make_path(partition), 'w') for partition in remaining}
-	counter = 0
-	for data in read(PATH_TO_NODES):
-		node_id = int(data[0])
-		node = nodes[node_id] # we need the node which knows the partition
-		partition_id = Find(node).id
-		data_row = ('\t'.join(data)).encode('UTF-8')
-		files[partition_id].write(data_row)
-		files[partition_id].write("\n")
-		counter += 1
-		if counter % BARRIER_WRITE_PARTS == 0: print "...writing nodes (%s)" % counter
-	[file.close() for file in files.values()]
-	print "Number of files written: %s" % len(files)
+	# print "\n>>> Start writing partitions..."
+	# shutil.rmtree(PATH_TO_OUTPUT_DIR, ignore_errors=True)
+	# os.mkdir(PATH_TO_OUTPUT_DIR)
+	# def make_path(partition):
+	# 	return os.path.join(PATH_TO_OUTPUT_DIR, '%s.csv' % partition)
+	# files = {partition: open(make_path(partition), 'w') for partition in remaining}
+	# counter = 0
+	# for data in read(PATH_TO_NODES):
+	# 	node_id = int(data[0])
+	# 	node = nodes[node_id] # we need the node which knows the partition
+	# 	partition_id = Find(node).id
+	# 	data_row = ('\t'.join(data)).encode('UTF-8')
+	# 	files[partition_id].write(data_row)
+	# 	files[partition_id].write("\n")
+	# 	counter += 1
+	# 	if counter % BARRIER_WRITE_PARTS == 0: print "...writing nodes (%s)" % counter
+	# [file.close() for file in files.values()]
+	# print "Number of files written: %s" % len(files)
 
 
 	########################### some information at end ############################
-	print "\n\n---------------------------------------------------------------------"
-	print ">>> I have written a file for every partition!"
-	print ">>> Note: I did not write the partition id to the data rows!"
-	print ">>> Note: I did not write headers to the files!"
+	# print "\n\n---------------------------------------------------------------------"
+	# print ">>> I have written a file for every partition!"
+	# print ">>> Note: I did not write the partition id to the data rows!"
+	# print ">>> Note: I did not write headers to the files!"
 
 
 
