cb32d7eed0b8251d59894b125c8fde98f90941e4
Wolff09 <s_wolff09@cs.uni-kl.de>
2013-03-05 00:27:15 +0100

    added writing of partition id to find_partitions

--- find_partitions.py
+++ find_partitions.py
@@ -10,11 +10,13 @@ from collections import Counter
 # configuration
 # PATH_TO_NODES = "private_data/musicbrainzGraph/musicbrainzNodes.csv"
 # PATH_TO_EDGES = "private_data/musicbrainzGraph/musicbrainzRelations.csv"
-# PATH_TO_OUTPUT_DIR = "private_data/musicbrainzGraph/own/"
+# PATH_TO_OUTPUT_NODES = "private_data/musicbrainzGraph/nodes.csv"
+# PATH_TO_OUTPUT_EDGES = "private_data/musicbrainzGraph/edges.csv"
 # test configuration
 PATH_TO_NODES = "public_data/find_partitions_test/nodes.csv"
 PATH_TO_EDGES = "public_data/find_partitions_test/edges.csv"
-PATH_TO_OUTPUT_DIR = "public_data/find_partitions_test/output/"
+PATH_TO_OUTPUT_NODES = "public_data/find_partitions_test/output_nodes"
+PATH_TO_OUTPUT_EDGES = "public_data/find_partitions_test/output_edges"
 
 
 # How talky shall I be ?
@@ -22,8 +24,8 @@ BARRIER_NODE_READING = 1000000
 BARRIER_OBJECT_CREATION = 500000
 BARRIER_MAKING_SETS = 500000
 BARRIER_EDGE_READING = 400000
-BARRIER_SEARCH_PARTS = 100000
-BARRIER_WRITE_PARTS = 80000
+BARRIER_NODE_WRITING = 200000
+BARRIER_EDGE_WRITING = 200000
 
 
 def main(length=0):
@@ -31,7 +33,8 @@ def main(length=0):
 	print ">>> I am finding your partitions!"
 	print ">>> nodes: %s" % PATH_TO_NODES
 	print ">>> edges: %s" % PATH_TO_EDGES
-	print ">>> output: %s" % PATH_TO_OUTPUT_DIR
+	print ">>> output nodes: %s" % PATH_TO_OUTPUT_NODES
+	print ">>> output edges: %s" % PATH_TO_OUTPUT_EDGES
 	print "---------------------------------------------------------------------\n"
 
 
@@ -40,23 +43,23 @@ def main(length=0):
 		print "\n>>> Starting to read nodes..."
 		for data in read(PATH_TO_NODES):
 			length += 1
-			if length % BARRIER_NODE_READING == 0: print "...reading nodes (%s)" % length
+			if length % BARRIER_NODE_READING == 0: print "     - %s" % length
 	print "Number of Nodes: %s" % length
 	
 
 	######################### creating UnionFind structure #########################
-	print "\n>>> Creating UninoFind Nodes..."
+	print "\n>>> Creating UnionFind Nodes..."
 	nodes = [i for i in range(0, length)] # create array of length length
 	for i in range(0, length):
 		nodes[i] = Node(i)
-		if i+1 % BARRIER_OBJECT_CREATION == 0: print "...creating Nodes (%s)" % i
+		if i+1 % BARRIER_OBJECT_CREATION == 0: print "     - %s" % i
 
 	print "\n>>> Making Initial UnionFind Sets..."
 	counter = 0
 	for node in nodes:
 		MakeSet(node)
 		counter += 1
-		if counter % BARRIER_MAKING_SETS == 0: print "...making Sets (%s)" % counter
+		if counter % BARRIER_MAKING_SETS == 0: print "     - %s" % counter
 
 
 	##################### merge partitions according to edges ######################
@@ -67,54 +70,52 @@ def main(length=0):
 		right = nodes[int(data[1])]
 		Union(left, right)
 		counter += 1
-		if counter % BARRIER_EDGE_READING == 0: print "...reading edges (%s)" % counter
+		if counter % BARRIER_EDGE_READING == 0: print "     - %s" % counter
 	print "Number of edges: %s" % counter
 
 	
-	########################## find remaining partitions ###########################
+	############################ search for partitions #############################
 	print "\n>>> Searching for partitions..."
-	sets = [Find(node).id for node in nodes]
-	c = Counter(sets)
-	# del nodes # release memory
-	# remaining = []
-	# counter = 0
-	# for p in sets:
-	#	if not p in remaining:
-	#		remaining += [p]
-	#	counter += 1
-	#	if counter % BARRIER_SEARCH_PARTS == 0: print "..searching for partitions (%s, %s)" % (counter, len(remaining))
-	# print "Number of Partitions: %s" % len(remaining)
-	# print "Partitions (id of one contained node): %s" % remaining
-	print "Number of Partitions: %s" % len(c)
-	print "Partitions (id of one contained node): \n%s" % str(c).replace(",", ",\n")
+	parents = [Find(node).id for node in nodes]
+
+	
+	############################ writing new node file #############################
+	print "\n>>> Writing partitions to nodes..."
+	counter = 0
+	with open(PATH_TO_OUTPUT_NODES, "w") as file:
+		for data in read(PATH_TO_NODES):
+			node_id = int(data[0])
+			partition_id = parents[node_id]
+			new_data = "%s\t%s\t%s\n" % (node_id, partition_id, "\t".join(data[1:]))
+			file.write(new_data)
+			counter += 1
+			if counter % BARRIER_NODE_WRITING == 0: print "     - %s" % counter
+		file.close()
+	print ">>> Note: I did not wrote a header to the file (column 2 is the partition id)."
 
 
-	##################### write partitions to dedicated files ######################
-	# print "\n>>> Start writing partitions..."
-	# shutil.rmtree(PATH_TO_OUTPUT_DIR, ignore_errors=True)
-	# os.mkdir(PATH_TO_OUTPUT_DIR)
-	# def make_path(partition):
-	# 	return os.path.join(PATH_TO_OUTPUT_DIR, '%s.csv' % partition)
-	# files = {partition: open(make_path(partition), 'w') for partition in remaining}
-	# counter = 0
-	# for data in read(PATH_TO_NODES):
-	# 	node_id = int(data[0])
-	# 	node = nodes[node_id] # we need the node which knows the partition
-	# 	partition_id = Find(node).id
-	# 	data_row = ('\t'.join(data)).encode('UTF-8')
-	# 	files[partition_id].write(data_row)
-	# 	files[partition_id].write("\n")
-	# 	counter += 1
-	# 	if counter % BARRIER_WRITE_PARTS == 0: print "...writing nodes (%s)" % counter
-	# [file.close() for file in files.values()]
-	# print "Number of files written: %s" % len(files)
+	############################ writing new edge file #############################
+	print "\n>>> Writing partitions to edges..."
+	counter = 0
+	with open(PATH_TO_OUTPUT_EDGES, "w") as file:
+		for data in read(PATH_TO_EDGES):
+			node_left = int(data[0])
+			partition_id = parents[node_left]
+			new_data = "%s\t%s\t%s\t%s\n" % (node_left, data[1], partition_id, "\t".join(data[2:]))
+			file.write(new_data)
+			counter += 1
+			if counter % BARRIER_EDGE_WRITING == 0: print "     - %s" % counter
+		file.close()
+	print ">>> Note: I did not wrote a header to the file (column 3 is the partition id)."
 
 
-	########################### some information at end ############################
-	# print "\n\n---------------------------------------------------------------------"
-	# print ">>> I have written a file for every partition!"
-	# print ">>> Note: I did not write the partition id to the data rows!"
-	# print ">>> Note: I did not write headers to the files!"
+	################# statistical information about the partitions #################
+	print "\n>>> Statistical Information..."
+	count_parents = Counter(parents)
+	print "Number of Partitions: %s" % len(count_parents)
+	print "PartitionId x PartitionSize: \n     - %s" % str(count_parents).replace(",", ",\n               ")
+	count_size = Counter(count_parents.values())
+	print "PartitionSize x NumberOfOccurrences: \n     - %s" % str(count_size).replace(",", ",\n               ")
 
 
 
