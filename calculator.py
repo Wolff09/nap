fc0e172c980336d5a118454c33260aea5be1a892
Sebastian <s_hennings09@cs.uni-kl.de>
2013-03-10 15:25:58 +0100

    Getestet, läuft jetzt. RAM allerdings zu klein... Tabellen müssen erst erstellt werden, siehe peewee Doc zu create_table()

--- calculator.py
+++ calculator.py
@@ -12,21 +12,29 @@ And for every node:
 
 from models import Partition, Node
 import component_iterator as iterator
-import shortcuts
+import shortcuts as sc
+import networkx as nx
 
 def calculate(path):
 	counter = 0
 	for graph in iterator.components(path):
-		pid = graph.Graph["pid"]
-		part = Partition(pid, diameter(graph), graph.number_of_nodes(), nx.density(graph))
+		pid = graph.graph["pid"]
+		# Because networkx sucks
+		if graph.number_of_edges() == 0:
+			part = Partition(pid=pid, diameter=0, num_nodes=graph.number_of_nodes(), density=nx.density(graph))
+			Node.create(nid=int(graph.nodes()[0]), pid=pid, degree=0, closeness=0, eccentricity=0, betweenness=0)
+			continue
+		else:
+			part = Partition(pid=pid, diameter=nx.diameter(graph), num_nodes=graph.number_of_nodes(), density=nx.density(graph))
+
 		part.save()
 
-		degree = degree_centrality(graph)
-		close = closeness_centrality(graph)
-		between = betweenness_centrality(graph)
-		ecc = eccentricity(graph)
+		degree = sc.degree_centrality(graph)
+		close = sc.closeness_centrality(graph)
+		between = sc.betweenness_centrality(graph)
+		ecc = sc.eccentricity(graph)
 		for node in graph.nodes():
-			n = Node(int(node),  pid, degree[node], close[node], 1/ecc[node], between[node])
+			n = Node(nid=int(node),  pid=pid, degree=degree[node], closeness=close[node], eccentricity=1/ecc[node], betweenness=between[node])
 			n.save()
 		counter += 1
 		if counter % 1000 == 0:
