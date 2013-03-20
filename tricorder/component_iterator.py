e1c68d440a1044c8671a7708791ad37a10e52fde
Wolff09 <s_wolff09@cs.uni-kl.de>
2013-03-20 13:24:18 +0100

    hopefully improved memory consumption

--- tricorder/component_iterator.py
+++ tricorder/component_iterator.py
@@ -12,28 +12,24 @@ def components(path_merge, delimiter="\t"):
 
 	Each graph contains the pid of the connected component.
 	"""
-	current_data = []
+	current_graph = None
 	current_pid = -1
 	for row in read(path_merge):
 		pid = int(row[0])
 		if pid != current_pid:
-			if current_data:
-				yield make_graph(current_pid, current_data, delimiter)
-			current_data = []
+			if current_graph:
+				yield current_graph
 			current_pid = pid
-		current_data += [(row[1], row[2])]
-	del current_data
+			current_graph = nx.Graph(pid=current_pid)
+		add_to_graph(current_graph, row[1], row[2])
 
-def make_graph(pid, data, delimiter):
-	graph = nx.Graph(pid=pid)
-	for type, data in data:
-		if type == "n":
-			parsed = parse(data, 5, delimiter)
-			graph.add_node(int(parsed[0]), type=parsed[3], name=parsed[4])
-		elif type == "e":
-			parsed = parse(data, 4, delimiter)
-			graph.add_edge(int(parsed[0]), int(parsed[1]), type=int(parsed[2]), name=parsed[3])
-	return graph
+def add_to_graph(graph, type, data, delimiter="\t"):
+	if type == "n":
+		parsed = parse(data, 5, delimiter)
+		graph.add_node(int(parsed[0]), type=parsed[3], name=parsed[4])
+	elif type == "e":
+		parsed = parse(data, 4, delimiter)
+		graph.add_edge(int(parsed[0]), int(parsed[1]), type=int(parsed[2]), name=parsed[3])
 
 def parse(data, length, delimiter):
 	return data.strip().split(delimiter, length)
